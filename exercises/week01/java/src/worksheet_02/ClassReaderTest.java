b729cf95fe05b3f03e95f5c6ca935792b92abd9c
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-12 21:00:20 +0000

    DRY out tests

--- exercises/week01/java/src/worksheet_02/ClassReaderTest.java
+++ exercises/week01/java/src/worksheet_02/ClassReaderTest.java
@@ -2,29 +2,28 @@ package worksheet_02;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ClassReaderTest {
-
-	@Test
-	public void testPrintClass() throws ClassNotFoundException {
-		String input = "java.lang.String";
+	
+	Class inputClass;
+	String input;
+	
+	@Before public void setup() throws ClassNotFoundException {
+		input = "java.lang.String";
+		inputClass = Class.forName(input);
+	}
+	
+	@Test public void testPrintClass() throws ClassNotFoundException {
 		ClassReader.printClass(input);
 	}
 
-	@Test
-	public void testGetSimpleName() throws ClassNotFoundException {
-		String input = "java.lang.String";
-		Class inputClass = Class.forName(input);
-
+	@Test public void testGetSimpleName() throws ClassNotFoundException {
 		assertEquals(ClassReader.getSimpleName(inputClass), "String");
 	}
 	
-	@Test
-	public void testGetModifiers() throws ClassNotFoundException {
-		String input = "java.lang.String";
-		Class inputClass = Class.forName(input);
-
+	@Test public void testGetModifiers() throws ClassNotFoundException {
 		assertEquals(ClassReader.getModifiers(inputClass), 17);
 	}
 	
