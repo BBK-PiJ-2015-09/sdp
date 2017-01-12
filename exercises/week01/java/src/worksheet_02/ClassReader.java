52cbbb233b94859554beac2940b3dcb7060bace1
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-12 20:17:07 +0000

    Add simpleName and getConstructors calls

--- exercises/week01/java/src/worksheet_02/ClassReader.java
+++ exercises/week01/java/src/worksheet_02/ClassReader.java
@@ -1,13 +1,22 @@
 package worksheet_02;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ClassReader {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Please enter a class name:");
-		String className = s.next();		
-		System.out.println(className);
+		String input = s.next();
+		Class inputClass = Class.forName(input);
+		
+		String simpleName = inputClass.getSimpleName();
+		System.out.println("Simple class name: " + simpleName);
+		
+        Constructor[] constructors = inputClass.getConstructors();
+        System.out.println("Constructors: " + Arrays.toString(constructors));
+		
 		s.close();
 	}
 }
