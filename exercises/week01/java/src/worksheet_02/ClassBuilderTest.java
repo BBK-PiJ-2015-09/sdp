4beb2a0a7f609724a3a66df00d80f9c564d15c86
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-12 22:04:20 +0000

    Add initial ClassBuilder test

---
+++ exercises/week01/java/src/worksheet_02/ClassBuilderTest.java
@@ -0,0 +1,29 @@
+package worksheet_02;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ClassBuilderTest {
+	Class inputClass;
+	String input;
+	
+	@Before public void setup() throws ClassNotFoundException {
+		input = "java.lang.String";
+		inputClass = Class.forName(input);
+	}
+
+	@Test public void consolePrintClass() throws ClassNotFoundException {
+		// Print output to console for development purposes.
+		ClassBuilder.buildClass(input);
+	}
+	
+}
