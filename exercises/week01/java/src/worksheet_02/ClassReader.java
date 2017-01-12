7a7332e1bc815d2d62f3aded88311705ec3e322c
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-12 19:56:09 +0000

    Add DummyClass

--- exercises/week01/java/src/worksheet_02/ClassReader.java
+++ exercises/week01/java/src/worksheet_02/ClassReader.java
@@ -2,6 +2,7 @@ package worksheet_02;
 
 public class ClassReader {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		String className = "DummyClass";
+		System.out.println(className);
 	}
 }
