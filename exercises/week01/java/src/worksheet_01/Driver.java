9731bf04275e6e47abe07223680c1ecd1c82ee08
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-12 19:43:03 +0000

    Create variables

--- exercises/week01/java/src/worksheet_01/Driver.java
+++ exercises/week01/java/src/worksheet_01/Driver.java
@@ -2,6 +2,7 @@ package worksheet_01;
 
 public class Driver {
 	public static void main(String[] args) {
-		// YOUR CODE GOES HERE
+		Storage<BankAccount> aStorage = new Storage<>();
+		Storage<String>	     sStorage = new Storage<>();
 	}
 }
