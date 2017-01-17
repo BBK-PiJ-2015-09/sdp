f7724f00a5e34628a36a78b12dc6f666517dce17
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-17 22:53:33 +0000

    Make test work

--- exercises/week01/java/src/worksheet_02/StudentClass.java
+++ exercises/week01/java/src/worksheet_02/StudentClass.java
@@ -1,5 +1,10 @@
 package worksheet_02;
 
 public class StudentClass {
+	public String a;
+	public String b;
+	public String c;
+	public String d;
+
 
 }
--- exercises/week01/java/src/worksheet_02/StudentClassTest.java
+++ exercises/week01/java/src/worksheet_02/StudentClassTest.java
@@ -2,14 +2,16 @@ package worksheet_02;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
+
 import org.junit.Test;
 
 public class StudentClassTest {
 
 	@Test
 	public void testMaxFourFields() {
-		StudentClass sclass = new StudentClass();
-		assert(sclass.getClass().getFields().length <= 4);
+		StudentClass sclass = new StudentClass();		
+		assertTrue(sclass.getClass().getFields().length <= 4);
 	}
 
 }
