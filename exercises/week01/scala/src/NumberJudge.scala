132640c1fd98551c418858f8548ff8a424f4e6bd
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-18 23:51:25 +0000

    Add NumberJudge

---
+++ exercises/week01/scala/src/NumberJudge.scala
@@ -0,0 +1,16 @@
+import scala.io.StdIn.readLine
+
+object NumberJudge {
+
+  def main(args: Array[String]): Unit = {
+    print("Please enter a number: ")
+    var input = readLine().toInt
+    if(input > 100) {
+      println("That's a big number")
+    } else {
+      println("That's a small number")
+    }
+
+  }
+
+}
