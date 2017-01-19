349947e4a02fb2ab0e8602ee9567c8b6ea8bf66e
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-19 22:51:29 +0000

    Add static report

--- exercises/week01/scala/src/worksheet_02/Hammurabi.scala
+++ exercises/week01/scala/src/worksheet_02/Hammurabi.scala
@@ -15,24 +15,42 @@ object Hammurabi extends App {
     var plagueDeaths = 0
 
     printIntroductoryMessage()
+    printReport()
+  }
+
+  def printReport(): Unit = {
+    println(
+      """
+        |O great Hammurabi!
+        |You are in year 1 of your ten year rule.
+        |In the previous year 0 people starved to death.
+        |In the previous year 5 people entered the kingdom.
+        |The population is now 100.
+        |We harvested 3000 bushels at 3 bushels per acre.
+        |Rats destroyed 200 bushels, leaving 2800 bushels in storage.
+        |The city owns 1000 acres of land.
+        |Land is currently worth 19 bushels per acre.
+        |There were 0 deaths from the plague.
+      """.stripMargin
+    )
   }
 
   def printIntroductoryMessage(): Unit = {
     println(
       """
-        Congratulations, you are the newest ruler of ancient Samaria, elected
-        for a ten year term of office. Your duties are to dispense food, direct
-        farming, and buy and sell land as needed to support your people. Watch
-        out for rat infestations and the plague! Grain is the general currency,
-        measured in bushels. The following will help you in your decisions:
-
-          * Each person needs at least 20 bushels of grain per year to survive.
-          * Each person can farm at most 10 acres of land.
-          * It takes 2 bushels of grain to farm an acre of land.
-          * The market price for land fluctuates yearly.
-
-        Rule wisely and you will be showered with appreciation at the end of
-        your term. Rule poorly and you will be kicked out of office!
+        |Congratulations, you are the newest ruler of ancient Samaria, elected
+        |for a ten year term of office. Your duties are to dispense food, direct
+        |farming, and buy and sell land as needed to support your people. Watch
+        |out for rat infestations and the plague! Grain is the general currency,
+        |measured in bushels. The following will help you in your decisions:
+        |
+        |  * Each person needs at least 20 bushels of grain per year to survive.
+        |  * Each person can farm at most 10 acres of land.
+        |  * It takes 2 bushels of grain to farm an acre of land.
+        |  * The market price for land fluctuates yearly.
+        |
+        |Rule wisely and you will be showered with appreciation at the end of
+        |your term. Rule poorly and you will be kicked out of office!
       """.stripMargin
 
     )
