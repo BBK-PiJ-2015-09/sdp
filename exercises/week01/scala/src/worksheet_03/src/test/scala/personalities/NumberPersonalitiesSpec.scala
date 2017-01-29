6d6f3b0b65746f2808e32de1bc40287e106b34a4
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-01-29 15:02:18 +0000

    Add more tests to check isPrime

--- exercises/week01/scala/src/worksheet_03/src/test/scala/personalities/NumberPersonalitiesSpec.scala
+++ exercises/week01/scala/src/worksheet_03/src/test/scala/personalities/NumberPersonalitiesSpec.scala
@@ -7,8 +7,18 @@ class NumberPersonalitiesSpec extends FunSuite {
 
   test("#isPrime") {
     assert(isPrime(1) == false)
+    assert(isPrime(2) == true)
+    assert(isPrime(3) == true)
     assert(isPrime(4) == false)
+    assert(isPrime(5) == true)
+    assert(isPrime(6) == false)
     assert(isPrime(7) == true)
+    assert(isPrime(8) == false)
+    assert(isPrime(9) == false)
+    assert(isPrime(10) == false)
+    assert(isPrime(997) == true)
+    assert(isPrime(998) == false)
+
   }
 
 }
