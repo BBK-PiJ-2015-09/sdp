7071f0626243624a827aa5794be6668c4e6a415e
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-02-02 22:46:22 +0000

    head test passing

--- exercises/week04/src/main/scala/functions/functions.scala
+++ exercises/week04/src/main/scala/functions/functions.scala
@@ -26,7 +26,9 @@ object Funcs {
     * @return a list whose head is `a' and whose tail is all but the first
     *         element of ls.
     **/
-  def setHead[A](ls: List[A], a: A): List[A] = ???
+  def setHead[A](ls: List[A], a: A): List[A] = {
+    a::tail(ls)
+  }
 
   /**
     * drop removes n elements from the given list. If n is greater than the
