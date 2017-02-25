583262b1a24ba23a56ea80eb7f5a575f90f7b42e
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-02-25 18:28:34 +0000

    Add test for LinInstruction#execute

--- coursework/cw-one/src/test/java/LinInstructionTest.java
+++ coursework/cw-one/src/test/java/LinInstructionTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.*;
 public class LinInstructionTest {
 
     @Test
+    public void test_execute() {
+        String label = "label";
+        String opcode = "lin";
+        int register = 0;
+        int value = 23;
+        Instruction instruction = new LinInstruction(label, register, value);
+        Machine m = new Machine();
+        Registers registers = new Registers();
+        m.setRegisters(registers);
+        instruction.execute(m);
+        assertEquals(m.getRegisters().getRegister(register), value);
+    }
+
+    @Test
     public void test_toString() {
         String label = "label";
         String opcode = "lin";
-        Integer register = 0;
-        Integer value = 0;
+        int register = 0;
+        int value = 0;
         Instruction instruction = new LinInstruction(label, register, value);
 
         assertEquals(instruction.toString(), label + ": " + opcode + " register " + register + " value is " + value);
