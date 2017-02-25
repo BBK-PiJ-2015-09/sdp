220fc9d044a180c8351d24f73aee9beb82e6718e
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-02-25 18:31:07 +0000

    Refactor LinInstructionTest for DRYness

--- coursework/cw-one/src/test/java/LinInstructionTest.java
+++ coursework/cw-one/src/test/java/LinInstructionTest.java
@@ -3,28 +3,29 @@ import static org.junit.Assert.*;
 
 public class LinInstructionTest {
 
+    String label = "label";
+    String opcode = "lin";
+    int register = 0;
+    int value = 23;
+    Instruction instruction;
+
+    @Before
+    public void buildInstruction() {
+        instruction = new LinInstruction(label, register, value);
+    }
+
     @Test
     public void test_execute() {
-        String label = "label";
-        String opcode = "lin";
-        int register = 0;
-        int value = 23;
-        Instruction instruction = new LinInstruction(label, register, value);
         Machine m = new Machine();
         Registers registers = new Registers();
         m.setRegisters(registers);
         instruction.execute(m);
+
         assertEquals(m.getRegisters().getRegister(register), value);
     }
 
     @Test
     public void test_toString() {
-        String label = "label";
-        String opcode = "lin";
-        int register = 0;
-        int value = 0;
-        Instruction instruction = new LinInstruction(label, register, value);
-
         assertEquals(instruction.toString(), label + ": " + opcode + " register " + register + " value is " + value);
     }
 }
