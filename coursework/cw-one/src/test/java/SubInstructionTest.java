b7a9bbe842eace0d5ec3629afd1a6d1441683178
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-02-26 16:18:22 +0000

    Add failing test for SubInstruction

---
+++ coursework/cw-one/src/test/java/SubInstructionTest.java
@@ -0,0 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SubInstructionTest {
+
+    String label = "label";
+    String opcode = "sub";
+    int result = 0;
+    int op1 = 1;
+    int op2 = 2;
+    int val1 = 1;
+    int val2 = 2;
+    int sum  = val1 - val2;
+    Instruction instruction;
+
+    @Before
+    public void buildInstruction() {
+        instruction = new SubInstruction(label, result, op1, op2);
+    }
+
+    @Test
+    public void test_execute() {
+        Machine m = new Machine();
+        Registers registers = new Registers();
+        m.setRegisters(registers);
+        m.getRegisters().setRegister(op1, val1);
+        m.getRegisters().setRegister(op2, val2);
+        instruction.execute(m);
+
+        assertEquals(sum, m.getRegisters().getRegister(result));
+    }
+
+    @Test
+    public void test_toString() {
+        assertEquals(label + ": " + opcode + " " + op1 + " - " + op2 + " to " + result, instruction.toString());
+    }
+}
