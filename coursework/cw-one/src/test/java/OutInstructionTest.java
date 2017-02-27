3cb7a0b6f758843aac6a37fd7c388c498318ea4b
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-02-27 14:15:01 +0000

    Remove redundant variables

--- coursework/cw-one/src/test/java/OutInstructionTest.java
+++ coursework/cw-one/src/test/java/OutInstructionTest.java
@@ -6,14 +6,12 @@ import static org.junit.Assert.assertEquals;
 public class OutInstructionTest {
 
     String label = "label";
-    String opcode = "out";
     int register = 0;
-    int value = 1;
     Instruction instruction;
 
     @Before
     public void buildInstruction() {
-        instruction = new OutInstruction(label, register, value);
+        instruction = new OutInstruction(label, register);
     }
 
     @Test
