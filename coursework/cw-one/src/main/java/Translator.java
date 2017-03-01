99eea5a9fbdc9acdf162945391f9f9bff9e57f4a
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-03-01 16:45:31 +0000

    Use reflection for Mul, Sub and Div

--- coursework/cw-one/src/main/java/Translator.java
+++ coursework/cw-one/src/main/java/Translator.java
@@ -85,6 +85,9 @@ public class Translator {
         ins = capitalise(ins);
         switch (ins) {
             case "Add":
+            case "Sub":
+            case "Mul":
+            case "Div":
                 r = scanInt();
                 s1 = scanInt();
                 s2 = scanInt();
@@ -101,27 +104,12 @@ public class Translator {
                 r = scanInt();
                 s1 = scanInt();
                 return new LinInstruction(label, r, s1);
-            case "Mul":
-                r = scanInt();
-                s1 = scanInt();
-                s2 = scanInt();
-                return new MulInstruction(label, r, s1, s2);
-            case "Sub":
-                r = scanInt();
-                s1 = scanInt();
-                s2 = scanInt();
-                return new SubInstruction(label, r, s1, s2);
             case "Bnz":
                 s1 = scanInt();
                 return new BnzInstruction(label, s1, scan());
             case "Out":
                 s1 = scanInt();
                 return new OutInstruction(label, s1);
-            case "Div":
-                r = scanInt();
-                s1 = scanInt();
-                s2 = scanInt();
-                return new DivInstruction(label, r, s1, s2);
         }
 
         // You will have to write code here for the other instructions.
