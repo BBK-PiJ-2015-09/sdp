156f1adde3a07e54a1b3310ed644c93856801d82
Harry Wright & Alex Rudall <hcawdev@gmail.com & alexrudall@gmail.com>
2017-03-19 14:04:51 +0000

    Initial fake implementation of a vendor parser

--- src/main/scala/factory/VirtualMachineFactory.scala
+++ src/main/scala/factory/VirtualMachineFactory.scala
@@ -1,7 +1,7 @@
 package factory
 
 import bc.{ByteCodeFactory, ByteCodeParser}
-import vendor.ProgramParser
+import vendor.{Instruction, ProgramParser}
 import vm.{VirtualMachine, VirtualMachineParser}
 
 /**
@@ -14,7 +14,47 @@ object VirtualMachineFactory {
   def byteCodeFactory: ByteCodeFactory = ???
 
   // TODO
-  def vendorParser: ProgramParser = ???
+  def vendorParser: ProgramParser = {
+    return new ProgramParser {
+      /**
+      * Parses a string representation of a bytecode program
+      * into an `InstructionList`.
+      *
+      * @param string the string to parse
+      * @return an instruction list
+      */
+    override def parseString(string: String) : InstructionList = {
+//      val input_array = string.split("/n") // Split string on newline
+//      var output_array = new Array[Instruction](input_array.length)
+//
+//      for (item <- input_array) {
+//        val split = item.split(" ")
+//        if (split.length == 2) {
+//          output_array :+ new Instruction(split(0), Vector(split(1).toInt))
+//        } else {
+//          output_array :+ new Instruction(split(0), Vector())
+//        }
+//      }
+//      println(output_array.length)
+//      output_array.to[InstructionList]
+
+      val i1 = new Instruction("iconst", Vector(4))
+      val i2 = new Instruction("iconst", Vector(5))
+      val i3 = new Instruction("iadd", Vector())
+      val i4 = new Instruction("print", Vector())
+      Vector(i1, i2, i3, i4)
+    }
+
+      /**
+        * Parses a file representation of a bytecode program
+        * into an `InstructionList`.
+        *
+        * @param file the file to parse
+        * @return an instruction list
+        */
+      override def parse(file: String) : InstructionList = ???
+    }
+  }
 
   // TODO
   def byteCodeParser: ByteCodeParser = ???
--- src/test/scala/vendor/PublicVendorParserSuite.scala
+++ src/test/scala/vendor/PublicVendorParserSuite.scala
@@ -8,7 +8,17 @@ class PublicVendorParserSuite extends FunSuite {
 
   test("[3] vendor parser should parse a program string correctly") {
     val insts = vp.parseString("iconst 4\niconst 5\niadd\nprint")
+
     assert(insts.length == 4)
+
+    assert(insts(0).name == "iconst")
+    assert(insts(0).args == Vector(4))
+    assert(insts(1).name == "iconst")
+    assert(insts(1).args == Vector(5))
+    assert(insts(2).name == "iadd")
+    assert(insts(2).args == Vector())
+    assert(insts(3).name == "print")
+    assert(insts(3).args == Vector())
   }
 
   test("[4] vendor parser should parse a program file correctly") {
