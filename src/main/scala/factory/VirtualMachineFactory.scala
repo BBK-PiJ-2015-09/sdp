506026863e4572cb364c2a73a190d7954967b13d
Harry Wright & Alex Rudall <hcawdev@gmail.com & alexrudall@gmail.com>
2017-03-19 17:51:18 +0000

    Implement ByteCodeParser

--- src/main/scala/factory/VirtualMachineFactory.scala
+++ src/main/scala/factory/VirtualMachineFactory.scala
@@ -94,7 +94,40 @@ object VirtualMachineFactory {
   }
 
   // TODO
-  def byteCodeParser: ByteCodeParser = ???
+  def byteCodeParser: ByteCodeParser = {
+    new ByteCodeParser {
+      /**
+        * Parses a vector of `Byte` into a vector of `ByteCode`.
+        *
+        * You should use [[ByteCodeValues.bytecode]] to help translate
+        * the individual `Byte`s into a corresponding [[ByteCode]].
+        *
+        * @param bc a vector of bytes representing bytecodes
+        * @return a vector of `ByteCode` objects
+        */
+      override def parse(bc: Vector[Byte]) : Vector[ByteCode] = {
+        val bcf = VirtualMachineFactory.byteCodeFactory
+        import scala.collection.mutable.ListBuffer
+        var byteCodeList = new ListBuffer[ByteCode]()
+
+        var iconst = false
+
+        for (code <- bc) {
+          if (iconst) {
+            byteCodeList += bcf.make(1, code)
+            iconst = false
+          } else {
+            if (code != 1) {
+              byteCodeList += bcf.make(code)
+            } else {
+              iconst = true
+            }
+          }
+        }
+        byteCodeList.toVector
+      }
+    }
+  }
 
   // TODO
   def virtualMachineParser: VirtualMachineParser = ???
