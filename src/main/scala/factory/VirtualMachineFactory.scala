7e378898cc5891ef5aa8c18d13c58db70481c671
Harry Wright & Alex Rudall <hcawdev@gmail.com & alexrudall@gmail.com>
2017-03-19 15:55:56 +0000

    Improve indentation

--- src/main/scala/factory/VirtualMachineFactory.scala
+++ src/main/scala/factory/VirtualMachineFactory.scala
@@ -17,28 +17,28 @@ object VirtualMachineFactory {
   def vendorParser: ProgramParser = {
     return new ProgramParser {
       /**
-      * Parses a string representation of a bytecode program
-      * into an `InstructionList`.
-      *
-      * @param string the string to parse
-      * @return an instruction list
-      */
-    override def parseString(string: String) : InstructionList = {
-      val input_array = string.split("\n") // Split string on newline
-      var output_array = new Array[Instruction](input_array.length)
+        * Parses a string representation of a bytecode program
+        * into an `InstructionList`.
+        *
+        * @param string the string to parse
+        * @return an instruction list
+        */
+      override def parseString(string: String) : InstructionList = {
+        val input_array = string.split("\n") // Split string on newline
+        var output_array = new Array[Instruction](input_array.length)
 
-      for (i <- 0 until input_array.length) {
-        val item = input_array(i)
-        val split = item.split(" ")
-        if (split.length == 2) {
-          output_array(i) = new Instruction(split(0), Vector(split(1).toInt))
-        } else {
-          output_array(i) = new Instruction(split(0), Vector())
+        for (i <- 0 until input_array.length) {
+          val item = input_array(i)
+          val split = item.split(" ")
+          if (split.length == 2) {
+            output_array(i) = new Instruction(split(0), Vector(split(1).toInt))
+          } else {
+            output_array(i) = new Instruction(split(0), Vector())
+          }
         }
-      }
 
-      output_array.to[Vector]
-    }
+        output_array.to[Vector]
+      }
 
       /**
         * Parses a file representation of a bytecode program
