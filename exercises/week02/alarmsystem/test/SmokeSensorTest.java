adc29ca392a82b4573f855008c1268b763bf2d5d
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-04-30 16:21:30 +0100

    Add failing test for SmokeSensor

--- exercises/week02/alarmsystem/test/SmokeSensorTest.java
+++ exercises/week02/alarmsystem/test/SmokeSensorTest.java
@@ -5,31 +5,52 @@ import static org.junit.Assert.assertEquals;
 public class SmokeSensorTest {
 
   @Test
-  public void testThatIsTriggeredReturnsFalse() {
+  public void testIsSometimesTriggered() {
     SmokeSensor sensor = new SmokeSensor();
-    boolean isTriggered = sensor.isTriggered();
-    assertEquals(false, isTriggered);
+    int attempts = 10;
+    int trueCount = 0;
+    for(int i = 1; i <= attempts; i++) {
+      if(sensor.isTriggered()) {
+        trueCount++;
+      }
+    }
+    assertEquals(1, trueCount);
   }
 
   @Test
-  public void testThatGetLocationReturnsNull() {
+  public void testGetLocation() {
     SmokeSensor sensor = new SmokeSensor();
     String location = sensor.getLocation();
-    assertEquals(null, location);
+    assertEquals("Kitchen", location);
   }
 
   @Test
-  public void testThatGetSensorTypeReturnsNull() {
+  public void testGetSensorType() {
     SmokeSensor sensor = new SmokeSensor();
     String sensorType = sensor.getSensorType();
-    assertEquals(null, sensorType);
+    assertEquals("SmokeSensor", sensorType);
   }
 
   @Test
-  public void testThatGetBatteryPercentageReturnsNegativeOne() {
+  public void testBatteryDrains() {
     SmokeSensor sensor = new SmokeSensor();
-    double batteryPercentage = sensor.getBatteryPercentage();
-    assertEquals(-1.0, batteryPercentage, 0.01);
+    int attempts = 5;
+    int expectedLevel = 100;
+    for(int i = 1; i <= attempts; i++) {
+      expectedLevel -= 20;
+      assertEquals(expectedLevel, sensor.getBatteryPercentage(), 0);
+    }
+  }
+  
+  @Test
+  public void testBatteryBottom() {
+    SmokeSensor sensor = new SmokeSensor();
+    int attempts = 20;
+    int expectedLevel = 0;
+    for(int i = 1; i <= attempts; i++) {
+      sensor.getBatteryPercentage();
+    }
+    assertEquals(expectedLevel, sensor.getBatteryPercentage(), 0);
   }
 
 }
