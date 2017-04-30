eecca836afe8bc4ebfbdbf75dbaceff2f833b406
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-04-30 15:41:08 +0100

    Add failing test for ex. 1

--- exercises/week02/alarmsystem/test/FireSensorTest.java
+++ exercises/week02/alarmsystem/test/FireSensorTest.java
@@ -6,30 +6,36 @@ import static org.junit.Assert.assertEquals;
 public class FireSensorTest {
 
   @Test
-  public void testThatIsTriggeredReturnsFalse() {
+  public void testSometimesReturnsTrue() {
     FireSensor sensor = new FireSensor();
-    boolean isTriggered = sensor.isTriggered();
-    assertEquals(false, isTriggered);
+    int attempts = 20;
+    int trueCount = 0;
+    for(int i = 1; i <= attempts; i++) {
+        if(sensor.isTriggered()) {
+            trueCount++;
+        }
+    }
+    assertEquals(1, trueCount);
   }
 
-  @Test
-  public void testThatGetLocationReturnsNull() {
-    FireSensor sensor = new FireSensor();
-    String location = sensor.getLocation();
-    assertEquals(null, location);
-  }
-
-  @Test
-  public void testThatGetSensorTypeReturnsNull() {
-    FireSensor sensor = new FireSensor();
-    String sensorType = sensor.getSensorType();
-    assertEquals(null, sensorType);
-  }
-
-  @Test
-  public void testThatGetBatteryPercentageReturnsNegativeOne() {
-    FireSensor sensor = new FireSensor();
-    double batteryPercentage = sensor.getBatteryPercentage();
-    assertEquals(-1.0, batteryPercentage, 0.01);
-  }
+//  @Test
+//  public void testThatGetLocationReturnsNull() {
+//    FireSensor sensor = new FireSensor();
+//    String location = sensor.getLocation();
+//    assertEquals(null, location);
+//  }
+//
+//  @Test
+//  public void testThatGetSensorTypeReturnsNull() {
+//    FireSensor sensor = new FireSensor();
+//    String sensorType = sensor.getSensorType();
+//    assertEquals(null, sensorType);
+//  }
+//
+//  @Test
+//  public void testThatGetBatteryPercentageReturnsNegativeOne() {
+//    FireSensor sensor = new FireSensor();
+//    double batteryPercentage = sensor.getBatteryPercentage();
+//    assertEquals(-1.0, batteryPercentage, 0.01);
+//  }
 }
