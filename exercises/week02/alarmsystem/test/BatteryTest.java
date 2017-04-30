d9c26cbef0322403279ace7eebe2f4d55270a11e
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-04-30 17:07:00 +0100

    Add failing BatteryTest

---
+++ exercises/week02/alarmsystem/test/BatteryTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BatteryTest {
+  Battery battery = new Battery(10);
+
+  @Test
+  public void testBatteryDrains() {
+    int calls = 10;
+    int expectedLevel = 100;
+    for(int i = 1; i <= calls; i++) {
+      expectedLevel -= 10;
+      assertEquals(expectedLevel, battery.call(), 0);
+    }
+  }
+
+  @Test
+  public void testBatteryBottom() {
+    int calls = 10;
+    int expectedLevel = 0;
+    for(int i = 1; i <= calls; i++) {
+      battery.getBatteryPercentage();
+    }
+    assertEquals(expectedLevel, battery.call(), 0);
+  }
+
+}
