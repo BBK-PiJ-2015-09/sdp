2bd907c9329cc6b345166ffb3545d0ade894dd6b
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-04-30 16:22:32 +0100

    Implement SmokeSensor

--- exercises/week02/alarmsystem/src/SmokeSensor.java
+++ exercises/week02/alarmsystem/src/SmokeSensor.java
@@ -1,21 +1,37 @@
 public class SmokeSensor implements Sensor {
+  private double battery = 100;
+  private int history = 1;
+
   @Override
   public boolean isTriggered() {
-    return false;
+    incrementHistory();
+    return history == 1;
   }
 
   @Override
   public String getLocation() {
-    return null;
+    return "Kitchen";
   }
 
   @Override
   public String getSensorType() {
-    return null;
+    return getClass().getSimpleName();
   }
 
   @Override
   public double getBatteryPercentage() {
-    return -1;
+    if (battery == 0) {
+      return battery;
+    } else {
+      return battery -= 20;
+    }
+  }
+
+  private void incrementHistory() {
+    if (history == 10) {
+      history = 1;
+    } else {
+      history++;
+    }
   }
 }
