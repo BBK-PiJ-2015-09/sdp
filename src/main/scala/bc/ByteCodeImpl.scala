665e4f228b0f6810a8952f64c9dfb4ef4898844f
Harry Wright & Alex Rudall <hcawdev@gmail.com & alexrudall@gmail.com>
2017-04-30 20:55:57 +0100

    Refactor ByteCodeImpl#execute methods into one-liners where possible

--- src/main/scala/bc/ByteCodeImpl.scala
+++ src/main/scala/bc/ByteCodeImpl.scala
@@ -2,9 +2,6 @@ package bc
 
 import vm.VirtualMachine
 
-/**
-  * Created by harrywright on 19/03/2017.
-  */
 class Iconst(arg: Int) extends ByteCode {
   override val code = bytecode("iconst")
   override def execute(vm: VirtualMachine) = vm.push(arg)
@@ -12,78 +9,42 @@ class Iconst(arg: Int) extends ByteCode {
 
 class Iadd extends ByteCode {
   override val code = bytecode("iadd")
-  override def execute(vm: VirtualMachine) = {
-    val x = vm.pop._1
-    val y = vm.pop._1
-    vm.push(x + y)
-  }
+  override def execute(vm: VirtualMachine) = vm.push(vm.pop._1 + vm.pop._1)
 }
 
 class Isub extends ByteCode {
   override val code: Byte = bytecode("isub")
-
-  override def execute(vm: VirtualMachine) = {
-    val x = vm.pop._1
-    val y = vm.pop._1
-    vm.push(x - y)
-  }
+  override def execute(vm: VirtualMachine) = vm.push(vm.pop._1 - vm.pop._1)
 }
 
 class Imul extends ByteCode {
   override val code: Byte = bytecode("imul")
-
-  override def execute(vm: VirtualMachine) = {
-    val x = vm.pop._1
-    val y = vm.pop._1
-    vm.push(x * y)
-  }
+  override def execute(vm: VirtualMachine) = vm.push(vm.pop._1 * vm.pop._1)
 }
 
 class Idiv extends ByteCode {
   override val code: Byte = bytecode("idiv")
-
-  override def execute(vm: VirtualMachine) = {
-    val x = vm.pop._1
-    val y = vm.pop._1
-    vm.push(x / y)
-  }
+  override def execute(vm: VirtualMachine) = vm.push(vm.pop._1 / vm.pop._1)
 }
 
 class Irem extends ByteCode {
   override val code: Byte = bytecode("irem")
-
-  override def execute(vm: VirtualMachine) = {
-    val x = vm.pop._1
-    val y = vm.pop._1
-    vm.push(x % y)
-  }
+  override def execute(vm: VirtualMachine) = vm.push(vm.pop._1 % vm.pop._1)
 }
 
 class Ineg extends ByteCode {
   override val code: Byte = bytecode("ineg")
-
-  override def execute(vm: VirtualMachine) = {
-    val x = vm.pop._1
-    vm.push(-x)
-  }
+  override def execute(vm: VirtualMachine) = vm.push(-vm.pop._1)
 }
 
 class Iinc extends ByteCode {
   override val code: Byte = bytecode("iinc")
-
-  override def execute(vm: VirtualMachine) = {
-    val x = vm.pop._1
-    vm.push(x + 1)
-  }
+  override def execute(vm: VirtualMachine) = vm.push(vm.pop._1 + 1)
 }
 
 class Idec extends ByteCode {
   override val code: Byte = bytecode("idec")
-
-  override def execute(vm: VirtualMachine) = {
-    val x = vm.pop._1
-    vm.push(x - 1)
-  }
+  override def execute(vm: VirtualMachine) = vm.push(vm.pop._1 - 1)
 }
 
 class Iswap extends ByteCode {
