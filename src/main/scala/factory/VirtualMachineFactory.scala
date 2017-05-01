bbbcaae561bb2b167d0119c585c6531c35f80c1c
Harry Wright <hcawdev@gmail.com>
2017-05-01 14:53:09 +0100

    added scalaDoc to VirtualMachineFactory

--- src/main/scala/factory/VirtualMachineFactory.scala
+++ src/main/scala/factory/VirtualMachineFactory.scala
@@ -5,20 +5,44 @@ import vendor.{ProgramParser, ProgramParserImpl}
 import vm.{VirtualMachine, VirtualMachineImpl, VirtualMachineParser, VirtualMachineParserImpl}
 
 /**
-  * The `VirtualMachineFactory` follows the *factory pattern*. It provides
-  * methods for each of the important parts in this assignment. You must
-  * implement each method such that it returns an object of the correct type.
+  * The [[VirtualMachineFactory]] follows the *factory pattern*.
+  * It provides methods which create and return instantiations of the
+  * required class
   */
 object VirtualMachineFactory {
-
+  /**
+    * Returns an instantiation of ByteCodeFactoryImpl
+    *
+    * @return ByteCodeFactoryImpl
+    */
   def byteCodeFactory: ByteCodeFactory = new ByteCodeFactoryImpl()
 
+  /**
+    * Returns an instantiation of ProgramParserImpl
+    *
+    * @return ProgramParserImpl
+    */
   def vendorParser: ProgramParser = new ProgramParserImpl()
 
+  /**
+    * Returns an instantiation of ByteCodeParserImpl
+    *
+    * @return ByteCodeParserImpl
+    */
   def byteCodeParser: ByteCodeParser = new ByteCodeParserImpl()
 
+  /**
+    * Returns an instantiation of VirtualMachineParserImpl
+    *
+    * @return VirtualMachineParserImpl
+    */
   def virtualMachineParser: VirtualMachineParser =  new VirtualMachineParserImpl()
 
+  /**
+    * Returns an instantiation of VirtualMachineImpl
+    *
+    * @return VirtualMachineImpl
+    */
   def virtualMachine: VirtualMachine = new VirtualMachineImpl()
 
 }
