e6b37e42ff30efff2194283dbc2ad1e3aaa00322
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-01 22:13:26 +0100

    Add failing test for ControlUnit

--- exercises/week02/alarmsystem/test/ControlUnitTest.java
+++ exercises/week02/alarmsystem/test/ControlUnitTest.java
@@ -1,3 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ControlUnitTest {
+
+  @Test
+  public void testAdd() {
+    ControlUnit controller = new ControlUnit();
+    controller.add(new FireSensor());
+    assertEquals(1, controller.sensors.length);
+  }
 
 }
