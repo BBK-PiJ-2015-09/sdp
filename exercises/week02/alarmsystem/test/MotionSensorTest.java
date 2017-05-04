72d3932ede573128d34fb99d110e6a6900183051
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-04 15:45:17 +0100

    Test MotionSensor for strategy use

--- exercises/week02/alarmsystem/test/MotionSensorTest.java
+++ exercises/week02/alarmsystem/test/MotionSensorTest.java
@@ -1,9 +1,21 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class MotionSensorTest {
-  MotionSensor sensor = new MotionSensor();
+  AlarmStrategy noiseStrategy = new NoiseStrategy();
+  List<AlarmStrategy> strategies = Arrays.asList(noiseStrategy);
+  MotionSensor sensor = new MotionSensor(strategies);
+
+  @Test
+  public void testStrategy() {
+    int attempts = 4;
+    for(int i = 1; i <= attempts; i++) { sensor.isTriggered(); }
+    assertEquals(true, noiseStrategy.active());
+  }
 
   @Test
   public void testIsSometimesTriggered() {
