5d1679ec8076de3f0c789358e6a5e6a805629e63
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-04 14:39:47 +0100

    Prevent security sensors from being triggered at night

--- exercises/week02/alarmsystem/src/SecurityControlUnit.java
+++ exercises/week02/alarmsystem/src/SecurityControlUnit.java
@@ -1,9 +1,39 @@
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class SecurityControlUnit extends ControlUnit {
 
   public SecurityControlUnit(List<Sensor> sensors) {
     super(sensors);
+  }
+
+  @Override
+  public void pollSensors() {
+    if (active()) {
+      for (Sensor sensor : sensors) {
+        if (sensor.isTriggered()) {
+          System.out.println("A " + sensor.getSensorType() + " sensor was triggered at " + sensor.getLocation());
+        } else {
+          System.out.println("Polled " + sensor.getSensorType() + " at " + sensor.getLocation() + " successfully");
+        }
+      }
+    }
+  }
+
+  private boolean active() {
+    return currentTime().isAfter(dawn()) && currentTime().isBefore(dusk());
+  }
+
+  private LocalDateTime dawn() {
+    return currentTime().withHour(06).withMinute(00).withSecond(00);
+  }
+
+  private LocalDateTime dusk() {
+    return currentTime().withHour(22).withMinute(00).withSecond(00);
+  }
+
+  public LocalDateTime currentTime() {
+    return LocalDateTime.now();
   }
 
 }
