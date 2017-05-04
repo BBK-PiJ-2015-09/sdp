70016f9b4d3b8cad53b92fe075fca56b753b80cd
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-04 22:19:58 +0100

    Test passing

--- exercises/week05/src/main/scala/counter/Counter.scala
+++ exercises/week05/src/main/scala/counter/Counter.scala
@@ -2,6 +2,6 @@ package counter
 
 class Counter(_count: Int) {
   def count = { _count }
-  def inc = { new Counter(count + 1) }
+  def inc(delta: Int = 1) = { new Counter(count + delta) }
   def dec = { new Counter(count - 1) }
 }
