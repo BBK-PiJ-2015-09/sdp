89ad700f5adcaa4f4f4b2bb5c90d99e1803769f3
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-04 21:36:35 +0100

    Complete ScalaTest 104

--- exercises/week05/src/test/scala/com/acme/PizzaSpec.scala
+++ exercises/week05/src/test/scala/com/acme/PizzaSpec.scala
@@ -2,24 +2,48 @@ package com.acme.pizza
 
 import org.scalatest.FunSpec
 import org.scalatest.BeforeAndAfter
+import org.scalatest.GivenWhenThen
 
-class PizzaSpec extends FunSpec with BeforeAndAfter {
+class PizzaSpec extends FunSpec with GivenWhenThen {
 
   var pizza: Pizza = _
 
-  before {
-    pizza = new Pizza
-  }
-
   describe("A Pizza") {
 
-    it("should start with no toppings") {
-      assert(pizza.getToppings.size == 1)
+    it("Should start with no toppings") {
+      Given("a new pizza")
+      pizza = new Pizza
+      Then("the topping count should be zero")
+      assert(pizza.getToppings.size == 0)
     }
 
-    it("should allow addition of toppings") (pending)
+    it("Should allow removal of toppings") {
+      Given("a new pizza with one topping")
+      pizza = new Pizza
+      pizza.addTopping(Topping("green olives"))
 
-    it("should allow removal of toppings") (pending)
+      When("the topping is removed")
+      pizza.removeTopping(Topping("green olives"))
+
+      Then("the topping count should be zero")
+      assert(0 === pizza.getToppings.size)
+
+    }
+
+    it ("should allow the addition of toppings") {
+      Given("a new pizza")
+      pizza = new Pizza
+
+      When("a topping is added")
+      pizza.addTopping(Topping("green olives"))
+
+      Then("the topping count should be incremented")
+      assert(1 === pizza.getToppings.size)
+
+      And("the topping should be what was added")
+      val t = pizza.getToppings(0)
+      assert(t === new Topping("green olives"))
+    }
   }
 
 }
