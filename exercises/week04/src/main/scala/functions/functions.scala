be06de1e417484b141830b09523e787cb7b00b32
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-04 17:23:20 +0100

    Add flatMap

--- exercises/week04/src/main/scala/functions/functions.scala
+++ exercises/week04/src/main/scala/functions/functions.scala
@@ -147,7 +147,10 @@ object Funcs {
     * @return a List[B] containing the flattened results of applying f to all
     *         elements of ls.
     */
-  def flatMap[A, B](ls: List[A])(f: A => List[B]): List[B] = ???
+  def flatMap[A, B](ls: List[A])(f: A => List[B]): List[B] = (ls) match {
+    case hd::Nil => f(hd)
+    case hd::tl => f(hd) ::: flatMap(tl)(f)
+  }
 
   // COMBINING FUNCTIONS
 
