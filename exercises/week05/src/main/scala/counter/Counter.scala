3675edcc471474bcb68d71e863b84504e711ad48
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-04 22:34:08 +0100

    Add Counter#adjust

--- exercises/week05/src/main/scala/counter/Counter.scala
+++ exercises/week05/src/main/scala/counter/Counter.scala
@@ -4,4 +4,5 @@ case class Counter(_count: Int = 0) {
   def count = { _count }
   def inc(delta: Int = 1) = { new Counter(count + delta) }
   def dec(delta: Int = 1) = { new Counter(count - delta) }
+  def adjust(adder: Adder) = { new Counter(adder.add(count)) }
 }
