cf8e8d683b3ac9f0a0badfd74f7b30cefc500ad6
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-05 12:28:25 +0100

    Add Person object

--- exercises/week05/src/main/scala/person/Person.scala
+++ exercises/week05/src/main/scala/person/Person.scala
@@ -15,3 +15,14 @@ class Person(_firstName: String, _lastName: String) {
   }
 
 }
+
+object Person {
+  def apply(firstName: String, lastName: String): Unit = {
+    new Person(firstName, lastName)
+  }
+
+  def apply(fullName: String) = {
+    val names = fullName.split(" ")
+    new Person(names(0), names(1))
+  }
+}
