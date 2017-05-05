3b8a6f4aad9bf4efea7cdac7a378cfafd1d98246
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-05 13:23:24 +0100

    Add Director companion object

--- exercises/week05/src/main/scala/film/Director.scala
+++ exercises/week05/src/main/scala/film/Director.scala
@@ -6,3 +6,13 @@ class Director(_firstName: String, _lastName: String, _dateOfBirth: Int) {
   def dateOfBirth() = { _dateOfBirth }
   def name() = { firstName + " " + lastName }
 }
+
+object Director {
+  def apply(firstName: String, lastName: String, dateOfBirth: Int): Director = {
+    new Director(firstName, lastName, dateOfBirth)
+  }
+
+  def older(a : Director, b : Director) = {
+    if (a.dateOfBirth < b.dateOfBirth) a else b
+  }
+}
