2881f4f9689b8e9587d90ad02a4abed8d7c50964
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-05 22:23:16 +0100

    Add initial attempt at Adapter pattern

---
+++ exercises/week07/src/main/scala/xpay/TestAdapter.scala
@@ -0,0 +1,22 @@
+package xpay
+
+object TestAdapter extends App {
+  // Setup.
+  val xpay: Xpay = new XpayImpl()
+  xpay.setCreditCardNo("12345678")
+  xpay.setCustomerName("Bob")
+  xpay.setCardExpMonth("Mar")
+  xpay.setCardExpYear("2018")
+  xpay.setCardCVVNo(999)
+  xpay.setAmount(100)
+
+  // Create adapter.
+  val payd: PayD = new XpaytoPayDAdapter(xpay)
+
+  // Test.
+  println(payd.getCustCardNo())
+  println(payd.getCardOwnerName())
+  println(payd.getCardExpMonthDate())
+  println(payd.getCVVNo)
+  println(payd.getTotalAmount)
+}
