f623363d414dedbdc18b36958bc7c416c8234154
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-05 12:46:37 +0100

    Add failing FilmTest

---
+++ exercises/week05/src/test/scala/film/FilmTest.scala
@@ -0,0 +1,39 @@
+package film
+
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+class FilmTest extends FunSuite with BeforeAndAfter {
+
+  var film: Film = _
+  var director: Director = _
+
+  before {
+    director = new Director("Cameron", "Crowe", 1957)
+    film = new Film("Almost Famous", 2000, 9.9, director)
+  }
+
+  test("name") {
+    assert(film.name() === "Almost Famous")
+  }
+
+  test("yearOfRelease") {
+    assert(film.yearOfRelease() === 2000)
+  }
+
+  test("imdbRating") {
+    assert(film.imdbRating() === 9.9)
+  }
+
+  test("director") {
+    assert(film.director() === director)
+  }
+
+  test("directorsAge") {
+    assert(film.directorsAge() === 43)
+  }
+
+  test("isDirectedBy") {
+    assert(film.isDirectedBy(director))
+  }
+
+}
