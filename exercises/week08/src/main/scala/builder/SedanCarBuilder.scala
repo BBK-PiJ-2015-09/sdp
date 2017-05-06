038d32cd09d6a5f2a6ca281a14e35a85fd3c6c72
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-06 20:19:57 +0100

    Add car details for Builder pattern

--- exercises/week08/src/main/scala/builder/SedanCarBuilder.scala
+++ exercises/week08/src/main/scala/builder/SedanCarBuilder.scala
@@ -3,19 +3,26 @@ package builder
 //TODO
 
 object SedanCarBuilder extends CarBuilder {
-  override def buildBodyStyle: Unit = ???
+  override def buildBodyStyle: Unit = {
+    """
+      |Body: External dimensions: overall length (inches): 202.9,
+      |overall width (inches): 76.2, overall height (inches): 60.7,
+      |wheelbase (inches): 112.9, front track (inches): 65.3,
+      |rear track (inches): 65.5 and curb to curb turning circle (feet): 39.5
+    """.stripMargin
+  }
 
-  override def buildPower: Unit = ???
+  override def buildPower: Unit = { "Power: 285 hp @ 6,500 rpm; 253 ft lb of torque @ 4,000 rpm" }
 
-  override def buildEngine: Unit = ???
+  override def buildEngine: Unit = {  "Engine: 3.5L Duramax V 6 DOHC" }
 
-  override def buildBreaks: Unit = ???
+  override def buildBreaks: Unit = { "Brakes: Four-wheel disc brakes: two ventilated. Electronic brake distribution" }
 
-  override def buildSeats: Unit = ???
+  override def buildSeats: Unit = { "Seats: Front seat centre armrest.Rear seat centre armrest.Split-folding rear seats" }
 
-  override def buildWindows: Unit = ???
+  override def buildWindows: Unit = { "Windows: Laminated side windows. Fixed rear window with defroster" }
 
-  override def buildFuelType: Unit = ???
+  override def buildFuelType: Unit = { "Fuel Type: Diesel 19 MPG city, 29 MPG highway, 23 MPG combined and 437 mi. range" }
 
   override def getCar: Car = ???
 }
--- exercises/week08/src/main/scala/builder/SportsCarBuilder.scala
+++ exercises/week08/src/main/scala/builder/SportsCarBuilder.scala
@@ -3,19 +3,29 @@ package builder
 // TODO
 
 object SportsCarBuilder extends CarBuilder {
-  override def buildBodyStyle: Unit = ???
+  override def buildBodyStyle: Unit = {
+    """
+      |Body: External dimensions: overall length (inches): 192.3,
+      |overall width (inches): 75.5, overall height (inches): 54.2,
+      |wheelbase (inches): 112.3, front track (inches): 63.7,
+      |rear track (inches): 64.1 and curb to curb turning circle (feet): 37.7
+    """.stripMargin }
 
-  override def buildPower: Unit = ???
+  override def buildPower: Unit = { "Power: 323 hp @ 6,800 rpm; 278 ft lb of torque @ 4,800 rpm" }
 
-  override def buildEngine: Unit = ???
+  override def buildEngine: Unit = { "Engine: 3.6L V 6 DOHC and variable valve timing" }
 
-  override def buildBreaks: Unit = ???
+  override def buildBreaks: Unit = { "Brakes: Four-wheel disc brakes: two ventilated. Electronic brake distribution.\nStability control" }
 
-  override def buildSeats: Unit = ???
+  override def buildSeats: Unit = {
+    """
+     |Seats: Driver sports front seat with one power adjustments manual height,
+     |front passenger seat sports front seat with one power adjustments
+    """.stripMargin }
 
-  override def buildWindows: Unit = ???
+  override def buildWindows: Unit = { "Windows: Front windows with one-touch on two windows" }
 
-  override def buildFuelType: Unit = ???
+  override def buildFuelType: Unit = { "Fuel Type: Petrol 17 MPG city, 28 MPG highway, 20 MPG combined and 380 mi. range" }
 
   override def getCar: Car = ???
 }
