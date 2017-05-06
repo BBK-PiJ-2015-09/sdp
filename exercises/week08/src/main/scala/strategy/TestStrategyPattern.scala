e7d5dcd339a8aad2da2b168ee3032c34fcf8fbd5
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-06 12:12:15 +0100

    Add answers to week08/question 1

--- exercises/week08/src/main/scala/strategy/TestStrategyPattern.scala
+++ exercises/week08/src/main/scala/strategy/TestStrategyPattern.scala
@@ -1,6 +1,9 @@
 package strategy
 
 object TestStrategyPattern extends App {
+  // 1. a) The strategy pattern allows the 'injection' of a strategy into an object, to give it different behaviour at runtime.
+  //    b) It makes sense to use the strategy pattern when an object has different, distinct behaviours.
+
   var formatter: TextFormatter = CapTextFormatter()
   var editor: TextEditor = TextEditor(formatter)
   editor.publishText("Testing text in caps formatter")
