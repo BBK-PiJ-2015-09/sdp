6966874d592cfc90157dde34e35bb4fa09670c2b
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-06 22:35:45 +0100

    Add answers to question 11

--- exercises/week08/src/main/scala/composite/TestCompositePattern.scala
+++ exercises/week08/src/main/scala/composite/TestCompositePattern.scala
@@ -1,6 +1,10 @@
 package composite
 
 object TestCompositePattern extends App {
+    // 11. a) The Composite pattern allows developers to 'compose' objects together to make a greater object which
+    //          has the same interface as any of its components.
+    //     b) You could use the composite pattern to DRY out a set of classes which are very similar.
+    //     c) The 4 participants of the composite design pattern are the Leaf, the Component, the Composite and the Client.
 
     val parentTag: HtmlTag = HtmlParentElement("<html>")
     parentTag.setStartTag("<html>")
