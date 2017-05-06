a8114c598dbbc45fdcf1cfa36a7418288aa2eeb1
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-06 11:22:14 +0100

    Update test to better fit requirements

--- exercises/week07/src/main/scala/decorator/PizzaDecorator.scala
+++ exercises/week07/src/main/scala/decorator/PizzaDecorator.scala
@@ -1,7 +1,7 @@
 package decorator
 
-public abstract class PizzaDecorator extends Pizza {
-  abstract def getDesc: String
+abstract class PizzaDecorator extends Pizza {
+  def getDesc: String
 
-  abstract def getPrice: Double
+  def getPrice: Double
 }
