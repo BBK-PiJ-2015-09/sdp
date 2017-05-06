99edea7851fcfa0a9f36bb5604be4f2287995263
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-06 20:01:38 +0100

    Add question 5

--- exercises/week08/src/main/scala/builder/TestBuilderPattern.scala
+++ exercises/week08/src/main/scala/builder/TestBuilderPattern.scala
@@ -1,6 +1,10 @@
 package builder
 
 object TestBuilderPattern {
+  // 5. As applications get more complex, objects and by extension their constructors may get larger - this is construction bloat.
+  // At a certain point it makes sense to break this logic out into a builder. Builders can be shared between objects with similar
+  // interfaces to permit more modular development.
+
   def main(args: Array[String]) {
     var carBuilder: CarBuilder = SedanCarBuilder
     var director: CarDirector = CarDirector(carBuilder)
