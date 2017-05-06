3cee773586ec8608c9b03d8b481b0b52fe73f7f1
Alex Rudall <arudal01@dcs.bbk.ac.uk>
2017-05-06 12:26:05 +0100

    Add answer to question 3

--- exercises/week08/src/main/scala/abstractfactory/TestAbstractFactoryPattern.scala
+++ exercises/week08/src/main/scala/abstractfactory/TestAbstractFactoryPattern.scala
@@ -1,6 +1,10 @@
 package abstractfactory
 
 object TestAbstractFactoryPattern {
+  // 3. It is appropriate to use the abstract factory pattern when there is a common interface but many ways to implement it,
+  // and the implementation can be done in different ways as long as the interface is adhered to. It allows the use of
+  // composition with different concrete factories via the common interface.
+
   def main(args: Array[String]) {
     var parserFactory: AbstractParserFactory = ParserFactoryProducer.getFactory("NYCFactory")
     var parser: XMLParser = parserFactory.getParserInstance("NYCORDER")
